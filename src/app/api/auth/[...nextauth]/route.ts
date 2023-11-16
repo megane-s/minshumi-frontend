81783a7baa848c11fba0d8c2770062b941f45e8a
TBSten <81161390+TBSten@users.noreply.github.com>
2023-11-17 05:59:25 +0900

    route handlerの型定義を修正

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,9 @@
 import { options } from "@/auth/server/options"
+import { } from "next"
 import NextAuth from "next-auth"
+import { NextRequest, NextResponse } from "next/server"
 
-const handler = NextAuth(options)
+const handler = NextAuth(options) as ((req:NextRequest)=>NextResponse)
 
 export { handler as GET, handler as POST }
+
