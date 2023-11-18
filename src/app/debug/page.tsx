3812beae78d09e334b4d22269e797b30f8aeffab
TBSten <81161390+TBSten@users.noreply.github.com>
2023-11-18 23:41:07 +0900

    確認用のページを表示

---
+++ src/app/debug/page.tsx
@@ -0,0 +1,87 @@
+"use client"
+
+import { developOnlyPage } from "@/util/developOnly";
+import { Button, Card, Checkbox, Container, Grid, Slider, Stack, Switch, Text, TextInput, Textarea, Title } from "@mantine/core";
+import Image from "next/image";
+
+export default function DebugPage() {
+    developOnlyPage()
+    return (
+        <Container>
+            <Stack p="lg" gap="lg">
+                <Grid>
+
+                    <Grid.Col span={12}>
+                        <Card>
+                            <Title mt="xl" mb="md">
+                                デバッグメニュー
+                            </Title>
+                            開発時のみ閲覧できるメニューです。
+                        </Card>
+                    </Grid.Col>
+
+                    <Grid.Col span={12}>
+                        <Card>
+                            <Title order={2}>
+                                カスタムテーマの確認
+                            </Title>
+                        </Card>
+                    </Grid.Col>
+
+                    <Grid.Col span={{ base: 12, sm: 6 }}>
+                        <Card>
+                            <Stack gap="md">
+                                <Title order={3}>ボタン</Title>
+                                <Button>
+                                    デフォルト
+                                </Button>
+                                <Button variant="light">
+                                    {'variant="light"'}
+                                </Button>
+                                <Button variant="outline">
+                                    {'variant="outline"'}
+                                </Button>
+                                <Button variant="gradient">
+                                    {'variant="gradient"'}
+                                </Button>
+                            </Stack>
+                        </Card>
+                    </Grid.Col>
+
+                    <Grid.Col span={{ base: 12, sm: 6 }}>
+                        <Card>
+                            <Stack gap="md">
+                                <Title order={3}>入力</Title>
+                                <TextInput placeholder="TextInput" />
+                                <Textarea rows={5} placeholder="Textarea" />
+                                <Slider />
+                                <Switch label="通知を受け取る" />
+                                <Checkbox label="利用規約に同意してあげる" />
+                            </Stack>
+                        </Card>
+                    </Grid.Col>
+
+                    <Grid.Col span={{ base: 12, sm: 4 }}>
+                        <Card>
+                            <Card.Section>
+                                <Image
+                                    src="/placeholder/300x200_blue.png"
+                                    alt="placeholder"
+                                    width={300} height={200}
+                                    style={{ width: "100%", height: "auto" }}
+                                />
+                            </Card.Section>
+                            <Text my="md">
+                                カード
+                            </Text>
+                            <Button variant="outline" w="fit-content">
+                                詳細
+                            </Button>
+                        </Card>
+                    </Grid.Col>
+
+                </Grid>
+            </Stack>
+        </Container>
+    )
+}
