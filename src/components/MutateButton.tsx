3cee717dd4f4d219bffb8bce4bcee419a0f15125
TBSten <81161390+TBSten@users.noreply.github.com>
2023-11-19 01:38:41 +0900

    MutateButtonを実装

---
+++ src/components/MutateButton.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useMutate } from "@/util/client/useMutate"
+import { Button, ButtonProps, Loader } from "@mantine/core"
+import { AnimatePresence, motion } from "framer-motion"
+
+interface MutateButtonProps<Res> extends ButtonProps {
+    mutation: ReturnType<typeof useMutate<() => Promise<Res>>>
+}
+const MutateButton = <Res,>({ mutation, children, leftSection, disabled, ...buttonProps }: MutateButtonProps<Res>) => {
+    return (
+        <Button
+            onClick={() => void mutation.mutate(null)}
+            leftSection={
+                <AnimatePresence initial={false}>
+                    {mutation.isLoading
+                        ? <motion.div
+                            initial="close"
+                            animate="open"
+                            exit="close"
+                            variants={{
+                                open: { opacity: 1, width: "auto", scale: 1.0 },
+                                close: { opacity: 0, width: 0, scale: 0.5 },
+                            }}
+                            transition={{ duration: 0.2 }}
+                        >
+                            <Loader size="sm" />
+                        </motion.div>
+                        : leftSection
+                    }
+                </AnimatePresence>
+            }
+            disabled={mutation.isLoading || disabled}
+            {...buttonProps}
+        >
+            {mutation.isLoading && mutation.loading
+                ? mutation.loading
+                : children
+            }
+        </Button>
+    )
+}
+
+export default MutateButton
