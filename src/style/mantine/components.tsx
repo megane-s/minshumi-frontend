4933baffdc0076be55311a33fd5c8a365e5c3cc1
greenmeganes <green.meganes@gmail.com>
2023-11-20 09:55:06 +0900

    themeを微調整

--- src/style/mantine/components.tsx
+++ src/style/mantine/components.tsx
@@ -1,9 +1,20 @@
-import { Card, MantineTheme } from "@mantine/core";
+import { Card, MantineTheme, TextInput, Textarea } from "@mantine/core";
 
 export const components: MantineTheme["components"] = {
     Card: Card.extend({
         defaultProps: {
             bg: "background.2",
+            withBorder: true,
         },
-    })
+    }),
+    TextInput: TextInput.extend({
+        defaultProps: {
+            styles: { input: { backgroundColor: "transparent" } }
+        }
+    }),
+    Textarea: Textarea.extend({
+        defaultProps: {
+            styles: { input: { backgroundColor: "transparent" } }
+        }
+    }),
 }
