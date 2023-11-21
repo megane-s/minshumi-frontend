fe739a506e918f008a9562b05fc1b2a56258d147
TBSten <81161390+TBSten@users.noreply.github.com>
2023-11-21 21:19:06 +0900

    login時のcallbackを指定できるように

--- src/app/login/LoginButton.tsx
+++ src/app/login/LoginButton.tsx
@@ -1,19 +1,22 @@
 "use client"
 
+import { login } from "@/auth/client/login"
 import MutateButton from "@/components/MutateButton"
 import { useMutate } from "@/util/client/useMutate"
-import { signIn } from "next-auth/react"
+import { useSearchParams } from "next/navigation"
 import { FC } from "react"
 
 interface LoginButtonProps {
 }
 const LoginButton: FC<LoginButtonProps> = () => {
-    const login = useMutate(async () => {
-        await signIn("google")
+    const searchParams = useSearchParams()
+    const handleLogin = useMutate(async () => {
+        const callbackUrl = searchParams.get("callback") ?? "/"
+        await login({ callbackUrl })
     })
     return (
         <MutateButton
-            mutation={login}
+            mutation={handleLogin}
             variant="filled"
             size="lg"
             w="100%"
