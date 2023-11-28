51e02c084ba8c996c606b42ba8764a11213c8160
TBSten <81161390+TBSten@users.noreply.github.com>
2023-11-29 04:46:16 +0900

    登録した好きな作品を取得する関数を追加

---
+++ src/art/recommend/getByUser.tsx
@@ -0,0 +1,11 @@
+import { UserId } from "@/user/type";
+import { RecommendArt } from "../type";
+import { prisma } from "@/prisma";
+
+export const getRecommendArtsByUser = async (userId: UserId): Promise<RecommendArt[]> => {
+    const recommendArts = await prisma.recommendArt.findMany({
+        include: { art: true },
+        where: { userId },
+    })
+    return recommendArts.map(({ art, likePoint }) => ({ ...art, likePoint }))
+}
