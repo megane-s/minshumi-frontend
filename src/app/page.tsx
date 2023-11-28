f0cc9179719386174fc77b8bf8941ea08cd54cfa
TBSten <81161390+TBSten@users.noreply.github.com>
2023-11-29 05:32:31 +0900

    検証用にアニメの作品一覧を表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import { getArtsWithTag } from "@/art/tag/getArts"
 
-export default function Home() {
+export default async function Home() {
+  const arts = await getArtsWithTag("アニメ")
+  console.log(arts)
   return (
     <main>
     </main>
