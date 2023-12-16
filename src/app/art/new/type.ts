c8839a20e90250a68993f864c0dbce1e650500fd
TBSten <81161390+TBSten@users.noreply.github.com>
2023-12-16 16:53:54 +0900

    不要な型定義を移動

--- src/app/art/new/type.ts
+++
@@ -1,7 +0,0 @@
-import { Art } from "@/art/type";
-
-export type InputRelatedArt = (
-    | Pick<Art, "title" | "imageUrl"> // 新規に登録する作品
-    | Art // すでに存在する作品
-)
-export type InputRelatedArts = InputRelatedArt[]
