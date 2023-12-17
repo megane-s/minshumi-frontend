407e63872d99d8adb8d16f9aa90ba450bcdb7196
TBSten <81161390+TBSten@users.noreply.github.com>
2023-12-17 15:53:32 +0900

    Switchを修正

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,10 +1,3 @@
-import { FC } from "react"
-import { Switch as MSwitch, SwitchProps as MSwitchProps } from "@mantine/core"
+import { Switch as MSwitch } from "@mantine/core"
 
-interface SwitchProps extends MSwitchProps {
-}
-export const Switch: FC<SwitchProps> = ({ ...props }) => {
-    return (
-        <MSwitch {...props} />
-    )
-}
+export const Switch = MSwitch
