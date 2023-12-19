f337440248d0caacf75ae1f150188c2c42a6a0c9
TBSten <81161390+TBSten@users.noreply.github.com>
2023-12-20 00:54:02 +0900

    環境変数名を更新

--- src/auth/server/options.ts
+++ src/auth/server/options.ts
@@ -6,11 +6,12 @@ import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const options: NextAuthOptions = {
+    secret: process.env._NEXTAUTH_SECRET,
     adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_AUTH_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET as string,
+            clientId: process.env._GOOGLE_AUTH_CLIENT_ID as string,
+            clientSecret: process.env._GOOGLE_AUTH_CLIENT_SECRET as string,
             authorization: {
                 params: {
                     prompt: "consent",
