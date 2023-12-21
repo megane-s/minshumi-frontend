302ef5d68d64fe27abdee4aca19eff165542d756
TBSten <81161390+TBSten@users.noreply.github.com>
2023-12-21 19:12:36 +0900

    作品タグ編集画面を実装

---
+++ src/app/art/[art_id]/edit/tag/EditArtTagForm.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { ArtTagForm } from "@/art/components/tag/ArtTagForm"
+import { getGenreTags, medias, others } from "@/art/components/tag/tags"
+import { ArtTag } from "@/art/type"
+import MutateButton from "@/components/MutateButton"
+import { useMutate } from "@/util/client/useMutate"
+import { sleep } from "@/util/sleep"
+import { Flex } from "@mantine/core"
+import { FC, useState } from "react"
+
+interface EditArtTagFormProps {
+    defaultValues: {
+        mediaTags: ArtTag[]
+        genreTags: ArtTag[]
+        otherTags: ArtTag[]
+    }
+}
+export const EditArtTagForm: FC<EditArtTagFormProps> = ({ defaultValues }) => {
+    const [selectedMedias, setSelectedMedias] = useState(defaultValues.mediaTags)
+    const [selectedGenres, setSelectedGenres] = useState(defaultValues.genreTags)
+    const [selectedOthers, setSelectedOthers] = useState(defaultValues.otherTags)
+
+    const handleSave = useMutate(async () => {
+        await sleep(1000)
+    }, {
+        onSuccess: { toast: "更新しました！" },
+        onError: { toast: "更新できませんでした..." },
+        loading: { toast: "更新中..." },
+    })
+    return (
+        <ArtTagForm
+            mediaTags={medias}
+            genreTags={getGenreTags(selectedMedias)}
+            otherTags={others}
+            selectedMediaTags={selectedMedias}
+            selectedGenreTags={selectedGenres}
+            selectedOtherTags={selectedOthers}
+            onChangeSelectedMediaTags={setSelectedMedias}
+            onChangeSelectedGenreTags={setSelectedGenres}
+            onChangeSelectedOtherTags={setSelectedOthers}
+            actions={
+                <Flex mt="lg" justify="center">
+                    <MutateButton variant="filled" size="lg" mutation={handleSave}>
+                        更新
+                    </MutateButton>
+                </Flex>
+            }
+        />
+    )
+}
---
+++ src/app/art/[art_id]/edit/tag/page.tsx
@@ -0,0 +1,29 @@
+import { getArt } from "@/art/get"
+import { EditArtTagForm } from "./EditArtTagForm"
+import { notFound } from "next/navigation"
+import { getTags } from "@/art/tag/getTags"
+import { getGenreTags, medias } from "@/art/components/tag/tags"
+
+interface PageProps {
+    params: { art_id: string }
+}
+const ArtEditTagPage = async ({ params: { art_id } }: PageProps) => {
+    const art = await getArt(art_id)
+    const tags = await getTags(art_id)
+    const mediaTags = medias.filter(media => tags.includes(media))
+    const genreTags = getGenreTags(medias).filter(genre => tags.includes(genre))
+    const otherTags = tags.filter(other => !mediaTags.includes(other) && !genreTags.includes(other))
+    if (!art) notFound()
+    return (
+        <div>
+            <EditArtTagForm
+                defaultValues={{
+                    mediaTags,
+                    genreTags,
+                    otherTags,
+                }}
+            />
+        </div>
+    )
+}
+export default ArtEditTagPage
