8de555ee7b50273ff0db6d991cdb9f53d59fae89
TBSten <81161390+TBSten@users.noreply.github.com>
2023-12-25 17:22:58 +0900

    初回ログイン時に /profile/new へジャンプする設定を追加

--- src/app/profile/new/page.tsx
+++ src/app/profile/new/page.tsx
@@ -1,8 +1,14 @@
 import { PageTitle } from "@/components/PageTitle"
 import { FirstRegisterTagsForm } from "./FirstRegisterTagsForm"
+import { getSession } from "@/auth/server/auth"
+import { redirect } from "next/navigation"
 
+export const dynamic = "force-dynamic"
 
 const FirstRegisterPage = async () => {
+    const session = await getSession()
+    console.log("firt register", session)
+    if (!session) redirect("/login")
     return (
         <div>
             <PageTitle my="lg">
