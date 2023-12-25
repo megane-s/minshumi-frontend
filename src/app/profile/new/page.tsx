76b4100d5a0d9e47039ed8336aa25d00ddc27266
TBSten <81161390+TBSten@users.noreply.github.com>
2023-12-25 17:03:00 +0900

    未使用の変数を削除

--- src/app/profile/new/page.tsx
+++ src/app/profile/new/page.tsx
@@ -1,13 +1,8 @@
 import { PageTitle } from "@/components/PageTitle"
 import { FirstRegisterTagsForm } from "./FirstRegisterTagsForm"
-import { getSession } from "@/auth/server/auth"
-import { notFound } from "next/navigation"
 
 
 const FirstRegisterPage = async () => {
-    const session = await getSession()
-    if (!session) notFound()
-    const userId = session.user.id
     return (
         <div>
             <PageTitle my="lg">
