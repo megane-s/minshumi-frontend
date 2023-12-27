8e079d48ece4ef564890efb9cf7a61a69c20a38e
TBSten <81161390+TBSten@users.noreply.github.com>
2023-12-27 14:57:18 +0900

    カルーセルの挙動を修正

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -2,9 +2,22 @@
 
 import '@mantine/carousel/styles.css'
 import { Carousel as MCarousel } from "@mantine/carousel"
+import { ComponentProps } from 'react'
+import { useClientRendered } from '@/util/client/useClientRendered'
+import { css, cx } from 'styled-system/css'
 // import Autoplay from 'embla-carousel-autoplay';
 
 // TODO オートプレイ機能
-export const Carousel = MCarousel
+export const Carousel = (props: ComponentProps<typeof MCarousel>) => {
+    const clientRendered = useClientRendered()
+    return (
+        <MCarousel
+            className={cx(css({ opacity: clientRendered ? 1 : "0", transition: "opacity 0.1s" }), props.className)}
+            {...props}
+        >
+            {props.children}
+        </MCarousel>
+    )
+}
 
 export const CarouselSlide = MCarousel.Slide
