cb77be4c2d61cd91ecb8ab93de0cce6fa29b2cf0
TBSten <81161390+TBSten@users.noreply.github.com>
2023-12-31 16:28:24 +0900

    ルートレイアウトを修正

--- src/app/BaseLayout/BaseLayout.tsx
+++ src/app/BaseLayout/BaseLayout.tsx
@@ -4,6 +4,7 @@ import { FC, ReactNode } from "react"
 import Header from "./Header"
 import Footer from "./Footer"
 import { AppShellMain } from "./AppShell"
+import { css } from "styled-system/css"
 
 interface BaseLayoutProps {
     children: ReactNode
@@ -15,7 +16,7 @@ const BaseLayout: FC<BaseLayoutProps> = ({ children }) => {
         >
             <Header />
             <AppShellMain>
-                <Container>
+                <Container className={css({ w: "full", minH: "full" })}>
                     {children}
                 </Container>
             </AppShellMain>
