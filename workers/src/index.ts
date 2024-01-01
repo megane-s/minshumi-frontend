2b12d692be56f97b4fcff285d73e12b6649f77b1
TBSten <81161390+TBSten@users.noreply.github.com>
2024-01-01 13:09:30 +0900

    workersでoriginも送信するよう修正

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -9,6 +9,8 @@ export interface Env {
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const reqUrl = new URL(request.url)
-		return fetch(`${env.ORIGIN_SERVER_URL}${reqUrl.pathname}${reqUrl.search}`, request)
+		const originReq = new Request(request)
+		originReq.headers.append("Origin", env.ORIGIN_SERVER_URL)
+		return fetch(`${env.ORIGIN_SERVER_URL}${reqUrl.pathname}${reqUrl.search}`, originReq)
 	},
 }
