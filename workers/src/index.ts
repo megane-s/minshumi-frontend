462c609e315c65c1340e6e52f2bb3f27782ce167
TBSten <81161390+TBSten@users.noreply.github.com>
2024-01-02 14:21:42 +0900

    リバースプロキシのホストを送信するよう修正

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -10,7 +10,8 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const reqUrl = new URL(request.url)
 		const originReq = new Request(request)
-		originReq.headers.append("Origin", env.ORIGIN_SERVER_URL)
-		return fetch(`${env.ORIGIN_SERVER_URL}${reqUrl.pathname}${reqUrl.search}`, originReq)
+		originReq.headers.set("X-Forwarded-Host", reqUrl.host)
+		const originRes = await fetch(`${env.ORIGIN_SERVER_URL}${reqUrl.pathname}${reqUrl.search}`, originReq)
+		return originRes
 	},
 }
