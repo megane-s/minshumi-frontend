497af6a3712b286f14aea0719c7dc386ca3799a7
TBSten <81161390+TBSten@users.noreply.github.com>
2024-01-10 20:15:00 +0900

    ログインしていなくても名刺ができるように暫定の対応

--- src/app/businesscard/new/actions.ts
+++ src/app/businesscard/new/actions.ts
@@ -2,12 +2,14 @@
 
 import { getSession } from "@/auth/server/auth"
 import { createBusinessCard } from "@/businessCard/create"
-import { notImplementError } from "@/util/notImplement"
 
 export const handleCreateBusinessCard = async (type: number) => {
     const session = await getSession()
-    if (!session) throw notImplementError(`名刺を作成するにはログインする必要があります`)
-    const newBusinessCard = await createBusinessCard(session.user.id, {
+    if (!session) {
+        // ログインしていない場合は名刺は保存しない
+        return
+    }
+    const newBusinessCard = await createBusinessCard(session?.user.id ?? null, {
         backgroundImageUrl: "https://storage.googleapis.com/minshumi-user-content/bg.png",
         canComment: false,
         imageUrl: "https://storage.googleapis.com/minshumi-user-content/tbsten500x500.png",
