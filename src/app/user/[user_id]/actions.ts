1ad2d856b3eac11c752f5bcd07e18bb84c21e7f0
TBSten <81161390+TBSten@users.noreply.github.com>
2024-01-10 20:18:50 +0900

    重複している関数を削除

--- src/app/user/[user_id]/actions.ts
+++ src/app/user/[user_id]/actions.ts
@@ -15,7 +15,6 @@ import { follow } from "@/user/follow/follow"
 import { getFollowings } from "@/user/follow/getFollowings"
 import { UserId } from "@/user/type"
 import { notImplementError } from "@/util/notImplement"
-import { serverAction } from "@/util/serverAction"
 import { revalidatePath } from "next/cache"
 
 const revalidateByBusinessCardId = async (businessCardId: BusinessCardId) => {
@@ -31,14 +30,6 @@ export const postComment = async (params: Omit<CreateBusinessCardCommentParams, 
     const newBusinessCardComment = await createBusinessCardComment({ ...params, commentUserId: loginUserId })
     await revalidateByBusinessCardId(newBusinessCardComment.businessCardId)
 }
-
-export const goodComment = serverAction(async (commentId: BusinessCardCommentId, by: UserId) => {
-    await goodToBusinessCardCommentGood(commentId, by)
-})
-
-export const cancelGoodComment = serverAction(async (commentId: BusinessCardCommentId, by: UserId) => {
-    await cancelGoodBusinessCardCommentGood(commentId, by)
-})
 
 export const updateComment = async (commentId: BusinessCardCommentId, params: UpdateBusinessCardComment) => {
     const session = await getSession()
