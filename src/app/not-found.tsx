90615715ad2b037848439256e89bdc860a09bf01
daisonmeganes <daison.meganes@gmail.com>
2024-01-15 14:10:32 +0900

    centerの位置調整

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,14 +4,15 @@ import Image from 'next/image'
 export default function NotFound() {
     return (
         <div>
-            <Image
-                src="/404.png"
-                alt='404'
-                width={800}
-                height={400}
-            />
-
             <center>
+                <Image
+                    src="/404.png"
+                    alt='404'
+                    width={800}
+                    height={400}
+                />
+
+
                 <p style={{ color: 'red' }}>
                     指定されたページが見つかりません
                 </p>
