e32723a51193b231b9940761db8b3d859ee37022
TBSten <81161390+TBSten@users.noreply.github.com>
2024-01-18 03:34:55 +0900

    設定画面も修正

--- src/art/tag/interest/update.ts
+++ src/art/tag/interest/update.ts
@@ -4,6 +4,9 @@ import { UserId } from "@/user/type"
 
 export const updateInterestTags = async (userId: UserId, tags: ArtTag[]) => {
     await prisma.$transaction(async prisma => {
+        await prisma.interestTag.deleteMany({
+            where: { userId },
+        })
         await Promise.all(tags.map(tag =>
             prisma.interestTag.upsert({
                 where: { userId_tag: { userId, tag } },
