6dc4003d19f24ef68105171181b642eb65f3a079
greenmeganes <green.meganes@gmail.com>
2024-01-18 16:47:00 +0900

    名刺削除ボタン（とりあえずボタンだけ）を追加

---
+++ src/app/businesscard/[businesscard_id]/edit/BusinessCardDelete.tsx
@@ -0,0 +1,20 @@
+import MutateButton from "@/components/MutateButton"
+import { useMutate } from "@/util/client/useMutate"
+import { FC } from "react"
+
+interface BusinessCardDeleteProps {
+}
+export const BusinessCardDelete: FC<BusinessCardDeleteProps> = () => {
+    const deleteBusinessCard = useMutate(async () => {
+        // TODO サーバアクションを呼び出す
+    }, {
+        loading: { toast: "削除中", button: "削除してる" },
+        onSuccess: { toast: "削除しました" },
+        onError: { toast: "削除できませんでした" },
+    })
+    return (
+        <MutateButton mutation={deleteBusinessCard} variant="light" color="error">
+            削除
+        </MutateButton>
+    )
+}
