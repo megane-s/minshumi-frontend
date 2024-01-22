dba688dbcc2c678c9dde6d25a60427a6a6b36a1b
TBSten <programmingcafeteria@gmail.com>
2024-01-22 11:21:11 +0900

    作品のいいねのアニメーションを実装

--- src/app/art/[art_id]/GoodButton.tsx
+++ src/app/art/[art_id]/GoodButton.tsx
@@ -1,12 +1,15 @@
 "use client"
 import { ActionIcon, Affix } from "@mantine/core"
-import { FC, useState } from "react"
+import { FC, ReactNode, useState } from "react"
 import { handleCancelGood, handleGood } from "./actions";
 import { ArtId } from "@/art/type";
 import { Dialog, useDialog } from "@/components/Dialog";
 import { Button } from "@/components/Button";
 import { login } from "@/auth/client/login";
 import { GoodIcon } from "@/components/icon/Good";
+import { AnimationControls, m, useAnimation } from "framer-motion";
+import { css } from "styled-system/css";
+import { useReward } from "react-rewards"
 
 interface GoodButtonProps {
     artId: ArtId
@@ -17,18 +20,28 @@ export const GoodButton: FC<GoodButtonProps> = ({ artId, isGooded: propsIsGooded
     const pleaseLoginDialog = useDialog()
 
     const [isGooded, setIsGooded] = useState(propsIsGooded)
+    const goodRewards = useGoodAnimation()
 
     //いいねしたときにログインしてなかったらダイアログを表示
     const handleGoodClick = async () => {
         if (!isLogined) return pleaseLoginDialog.onOpen()
-        if (!isGooded) {
-            setIsGooded(true)
-            await handleGood(artId);
-        } else {
-            setIsGooded(false)
-            await handleCancelGood(artId);
+
+        try {
+            if (!isGooded) {
+                setIsGooded(true)
+                await Promise.all([
+                    goodRewards.animate(),
+                    handleGood(artId),
+                ])
+            } else {
+                setIsGooded(false)
+                await handleCancelGood(artId);
+            }
+        } catch (e) {
+            console.error(e)
+            setIsGooded(isGooded)
         }
-    };
+    }
 
     return (
         <Affix position={{ bottom: 8, right: 8 }}>
@@ -41,10 +54,55 @@ export const GoodButton: FC<GoodButtonProps> = ({ artId, isGooded: propsIsGooded
                     onClick={() => void login()}>ログイン</Button>
             </Dialog>
 
-            <ActionIcon size="xl" variant={isGooded ? "filled" : "outline"} bg={isGooded ? "primary" : "background.2"} onClick={() => void handleGoodClick()}>
-                <GoodIcon />
+            <ActionIcon
+                size="xl"
+                variant={isGooded ? "filled" : "outline"}
+                bg={isGooded ? "primary" : "background.2"}
+                onClick={() => void handleGoodClick()}
+                className={css({ position: "relative" })}
+            >
+                <GoodRewards {...goodRewards.props}>
+                    <GoodIcon />
+                </GoodRewards>
             </ActionIcon>
 
         </Affix>
+    )
+}
+
+const useGoodAnimation = () => {
+    const good = useAnimation()
+    const rewardIds = {
+        1: "good-reward-1",
+    }
+    const reward1 = useReward(rewardIds[1], "confetti", { angle: 110, startVelocity: 20 })
+    const animate = async () => {
+        reward1.reward()
+    }
+    return {
+        animate,
+        props: {
+            good,
+            reward1Id: rewardIds[1],
+        },
+    }
+}
+
+interface GoodRewardsProps {
+    good: AnimationControls
+    reward1Id: string
+    children: ReactNode
+}
+export const GoodRewards: FC<GoodRewardsProps> = ({
+    good,
+    reward1Id,
+    children,
+}) => {
+    return (
+        <m.div animate={good}>
+            <div id={reward1Id} className={css({ position: "absolute", bottom: 0, left: 0, right: 0, mx: "auto", overflow: "visible" })} />
+            {children}
+        </m.div>
+
     )
 }
