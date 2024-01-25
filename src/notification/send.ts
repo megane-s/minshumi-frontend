affdd6696c98f2587649896b3952bb3ecefe1a3e
TBSten <programmingcafeteria@gmail.com>
2024-01-25 13:54:38 +0900

    コメントにいいねしたときの通知を実装

--- src/notification/send.ts
+++ src/notification/send.ts
@@ -1,22 +1,25 @@
-import { notImplementWarn } from "@/util/notImplement"
 import "server-only"
 import { z } from "zod"
-import { NotificationSchema } from "./type"
+import { prisma } from "@/prisma"
+import { UserIdSchema } from "@/user/type"
+import { UserCommentIdSchema } from "@/user/comment/type"
 
-export const SendNotificationParamsSchema = NotificationSchema.pick({
-    type: true,
-    userId: true,
-    content: true,
+export const SendGoodCommentNotificationParamsSchema = z.object({
+    sendUserId: UserIdSchema,
+    targetUserId: UserIdSchema,
+    commentId: UserCommentIdSchema,
 })
-export type SendNotificationParams = z.infer<typeof SendNotificationParamsSchema>
+export type SendGoodCommentNotificationParams = z.infer<typeof SendGoodCommentNotificationParamsSchema>
 
-/**
- * 未実装。
- * ユーザに通知を届ける。
- * @param params 送信する通知の内容。
- */
-// 未実装のためdisable。実装し次第外す。
-// eslint-disable-next-line unused-imports/no-unused-vars, @typescript-eslint/no-unused-vars
-export const sendNotification = async (params: SendNotificationParams): Promise<void> => {
-    notImplementWarn("sendNotification はまだ実装されていません。")
+export const sendGoodCommentNotification = async (params: SendGoodCommentNotificationParams): Promise<void> => {
+    await prisma.notification.create({
+        data: {
+            type: "user-comment-good",
+            content: JSON.stringify({
+                userId: params.sendUserId,
+                commentId: params.commentId,
+            }),
+            userId: params.targetUserId,
+        },
+    })
 }
--- src/user/comment/good/good.ts
+++ src/user/comment/good/good.ts
@@ -3,17 +3,31 @@ import "server-only";
 import { prisma } from "@/prisma";
 import { UserId } from "@/user/type";
 import { UserCommentId } from "../type";
+import { sendGoodCommentNotification } from "@/notification/send";
+import { getUserComment } from "../get";
+import { notImplementError } from "@/util/notImplement";
 
 /**
  * 名刺に対するコメントへいいねする。
  * @param commentId いいねするコメントのID。
  * @param by いいねするユーザのID。
  */
-export const goodToUserCommentGood = async (commentId: UserCommentId, by: UserId): Promise<void> => {
+export const goodToUserCommentGood = async (commentId: UserCommentId, by: UserId, notify: boolean = true): Promise<void> => {
+    const comment = await getUserComment(commentId)
+    if (!comment) {
+        throw notImplementError(`存在しないコメントにいいねしようとしました`)
+    }
     await prisma.userCommentGoodUser.create({
         data: {
             commentId,
             goodUserId: by,
         }
     })
+    if (notify) {
+        await sendGoodCommentNotification({
+            sendUserId: by,
+            targetUserId: comment.commentUserId,
+            commentId,
+        })
+    }
 }
