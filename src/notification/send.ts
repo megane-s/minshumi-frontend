7883df9fe6e82bc02a3c2b0da01d668233319d73
TBSten <programmingcafeteria@gmail.com>
2024-01-25 13:59:23 +0900

    コメントした時の通知を実装

--- src/notification/send.ts
+++ src/notification/send.ts
@@ -23,3 +23,23 @@ export const sendGoodCommentNotification = async (params: SendGoodCommentNotific
         },
     })
 }
+
+export const SendCommentNotificationParamsSchema = z.object({
+    commentUserId: UserIdSchema,
+    targetUserId: UserIdSchema,
+    commentId: UserCommentIdSchema,
+})
+export type SendCommentNotificationParams = z.infer<typeof SendCommentNotificationParamsSchema>
+
+export const sendCommentNotification = async (params: SendCommentNotificationParams) => {
+    await prisma.notification.create({
+        data: {
+            type: "comment",
+            content: JSON.stringify({
+                userId: params.commentUserId,
+                commentId: params.commentId,
+            }),
+            userId: params.targetUserId,
+        },
+    })
+}
