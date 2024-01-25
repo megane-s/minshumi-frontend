20eda8bb56ee4d2ad8609a356b379878d8c7e079
TBSten <programmingcafeteria@gmail.com>
2024-01-25 16:22:38 +0900

    通知の取得ロジックを修正

--- src/notification/get.ts
+++ src/notification/get.ts
@@ -7,8 +7,31 @@ import { Notification } from "./type";
  * @param userId 指定するユーザのID。
  * @returns 届いている通知一覧。
  */
-export const getNotifications = async (userId: UserId): Promise<Notification[]> => {
-    return await prisma.notification.findMany({
-        where: { userId },
-    })
+export const getNotifications = async (
+    userId: UserId,
+    params: Partial<{
+        recommend: Partial<{ limit: number }>
+        others: Partial<{ limit: number }>
+    }> = {},
+): Promise<{
+    recommends: Notification[]
+    others: Notification[]
+}> => {
+    const [recommends, others] = await Promise.all([
+        prisma.notification.findMany({
+            where: { type: "recommend", userId },
+            take: params.recommend?.limit ?? 5,
+            orderBy: { updateAt: "desc" },
+        }),
+        prisma.notification.findMany({
+            where: { type: { not: "recommend" }, userId },
+            take: params.others?.limit,
+            orderBy: { updateAt: "desc" },
+        }),
+    ])
+    return {
+        recommends,
+        others,
+    }
 }
+
