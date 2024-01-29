f0b0d09268d1b632a425dd61dc52e35e3e501cbf
TBSten <programmingcafeteria@gmail.com>
2024-01-29 16:29:38 +0900

    loadingを修正

--- src/app/notification/loading.tsx
+++ src/app/notification/loading.tsx
@@ -1,12 +1,15 @@
 import { CenterLoader } from "@/components/CenterLoader"
 import { Loader } from "@/components/Loader"
 import { FC } from "react"
+import { css } from "styled-system/css"
 
 interface NotificationLoadingProps {
 }
 const NotificationLoading: FC<NotificationLoadingProps> = () => {
   return (
-    <CenterLoader />
+    <CenterLoader
+      className={css({ my: "lg" })}
+    />
   )
 }
 export default NotificationLoading
