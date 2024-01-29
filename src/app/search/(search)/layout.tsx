dc083a46967c80eedf66025423f84dc507e3133e
TBSten <programmingcafeteria@gmail.com>
2024-01-29 15:20:50 +0900

    SearchBar周りのUIの修正

--- src/app/search/(search)/layout.tsx
+++ src/app/search/(search)/layout.tsx
@@ -3,7 +3,7 @@
 import SearchBar from "@/components/SearchBar"
 import { Space, Tabs } from "@mantine/core"
 import Link from "next/link"
-import { useSelectedLayoutSegment } from "next/navigation"
+import { useSearchParams, useSelectedLayoutSegment } from "next/navigation"
 import { ReactNode } from "react"
 import { MdStarBorder } from "react-icons/md";
 import { LiaGrinStarsSolid } from "react-icons/lia";
@@ -14,20 +14,23 @@ interface PageProps {
 }
 const SaearchLayout = ({ children }: PageProps) => {
     const segment = useSelectedLayoutSegment() as "art" | "user"
+    const q = useSearchParams().get("q")
     return (
         <div>
             <SearchBar
+                key={q}
                 type={segment}
+                defaultValue={q ?? ""}
             />
             <Tabs variant="default" radius="md" value={segment}>
                 <Tabs.List justify="center">
-                    <Link href="/search/art">
+                    <Link href={`/search/art?q=${q}`}>
                         <Tabs.Tab value="art" leftSection={<MdStarBorder />} className={css({ fontSize: "16px" })} >
                             作品
                         </Tabs.Tab>
                     </Link>
                     <Space w="1rem" />
-                    <Link href="/search/user">
+                    <Link href={`/search/user?q=${q}`}>
                         <Tabs.Tab value="user" leftSection={<LiaGrinStarsSolid />} className={css({ fontSize: "16px" })}>
                             ユーザー
                         </Tabs.Tab>
