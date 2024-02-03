e3ca627c857333e03defffa0e2f64e620fa0d0ab
TBSten <81161390+TBSten@users.noreply.github.com>
2024-02-03 23:04:52 +0900

    作品の検索をLIKEに変更

--- src/art/search.ts
+++ src/art/search.ts
@@ -1,27 +1,25 @@
-import { searchApiClient } from "@/search/client"
-import { notImplementError } from "@/util/notImplement"
+import { prisma } from "@/prisma"
 import { cache } from "react"
 import "server-only"
-import { getArt } from "./get"
 import { Art } from "./type"
 
 /**
- * 未実装。
  * 作品を検索する。
  * @param query 検索ワード。
  * @returns 検索結果の作品一覧。
  */
 export const searchArt = cache(async (query: string): Promise<Art[]> => {
-    const artIds = await searchApiClient.GET("/search/art", {
-        params: {
-            query: { q: query },
-        },
-    }).then(r => r.data)
-    if (!artIds) {
-        throw notImplementError("検索サーバのエラー")
-    }
-    const arts = await Promise.all(
-        artIds.map(artId => getArt(artId))
-    )
-    return arts.filter((art): art is Art => !!art)
+    const q_words = query.split(/\s/g)
+    const arts = await Promise.all(q_words.map(word =>
+        prisma.art.findMany({
+            where: {
+                OR: [
+                    { title: { contains: word } },
+                    { description: { contains: word } },
+                ]
+            },
+        })
+    ))
+    return arts.flat()
 })
+
