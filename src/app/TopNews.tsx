06627480b0d46fe3ac25d4971532c56a49cb0266
TBSten <programmingcafeteria@gmail.com>
2024-02-07 15:57:56 +0900

    アピールの説明を追加

--- src/app/TopNews.tsx
+++ src/app/TopNews.tsx
@@ -24,37 +24,57 @@ export const TopNews: FC<TopNewsProps> = () => {
     )
 }
 
-interface NewsSlideProps {
-    href?: string
-    src: ImageProps["src"]
-    alt: string
-    priority?: boolean
+interface AppealProps {
 }
-const NewsSlide: FC<NewsSlideProps> = ({ href, src, alt, priority = false }) => {
-    const content = (
-        <div className={flex({ flexDir: "column", justify: "center", align: "center" })}>
+export const Appeal: FC<AppealProps> = async () => {
+    const session = await getSession()
+    return (
+        <div className={flex({
+            textAlign: "center",
+            flexDir: "column",
+            alignItems: "center",
+            gap: { base: "lg", sm: "xl" },
+            w: "full", px: "md", py: { base: "xl", sm: "10" },
+            bgGradient: "to-br", gradientFrom: "primary.1", gradientTo: "primary.2",
+            color: "white",
+        })}>
+            <div className={flex({ flexDir: "column", align: "center" })}>
+                <div className={center({ w: "full", fontSize: { base: "2xl", sm: "5xl" }, fontWeight: "bold" })}>
+                    作品をアピールしよう
+                </div>
+                <div className={css({ my: "2", position: "relative", w: "fit-content", px: "xl", fontSize: { base: "md", sm: "2xl" } })}>
+                    作品をアピールすると <br />
+                    他の人に作品が届きます
+                    <Image
+                        src={TextPopImg}
+                        alt=""
+                        className={css({ position: "absolute", w: "full", height: "auto", bottom: "0", left: 0, right: 0, mx: "auto" })}
+                    />
+                </div>
+                <div className={css({ mt: "xl" })}>
+                    {session
+                        ? <LinkButton
+                            variant="white"
+                            className={css({ borderRadius: "full !important" })}
+                            size="xl"
+                            href="/art/appeal"
+                        >
+                            作品をアピール
+                        </LinkButton>
+                        : <LoginButton
+                            variant="white"
+                            className={css({ borderRadius: "full !important" })}
+                            size="xl"
+                            callbackUrl="/art/appeal"
+                        />
+                    }
+                </div>
+            </div>
             <Image
-                src={src}
-                alt={alt}
-                width={800}
-                height={450}
-                className={css({ width: "100%", height: "auto", objectFit: "contain" })}
-                priority={priority}
+                src={TopPanel1Img}
+                alt=""
+                width={300}
             />
         </div>
-    )
-    if (href) {
-        return (
-            <CarouselSlide>
-                <Link href={href}>
-                    {content}
-                </Link>
-            </CarouselSlide>
-        )
-    }
-    return (
-        <CarouselSlide>
-            {content}
-        </CarouselSlide>
     )
 }
