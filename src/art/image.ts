360fff9b16aecfb80340de8d78e2ed1fbe4deb6a
TBSten <81161390+TBSten@users.noreply.github.com>
2024-02-08 06:13:16 +0900

    作品のデフォ画像を別ファイルに移動

---
+++ src/art/image.ts
@@ -0,0 +1,24 @@
+export const images = [
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-1-1.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-1-2.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-1-3.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-1-4.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-1-5.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-1-6.png",
+
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-2-1.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-2-2.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-2-3.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-2-4.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-2-5.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-2-6.png",
+
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-3-1.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-3-2.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-3-3.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-3-4.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-3-5.png",
+    "https://storage.googleapis.com/minshumi-user-content/art-default/type-3-6.png",
+]
+
+export const getRandomArtImage = () => images[Math.floor(Math.random() * images.length)]
